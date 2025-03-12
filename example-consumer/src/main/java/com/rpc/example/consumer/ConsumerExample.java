faa39a3cc3c6aa732a7b2a8a5e1f82eb6f3113b4
StarlitDreamer <3435185298@qq.com>
2025-03-12 16:42:29 +0800

    完成第二章

---
+++ example-consumer/src/main/java/com/rpc/example/consumer/ConsumerExample.java
@@ -0,0 +1,17 @@
+package com.rpc.example.consumer;
+
+import com.rpc.myrpc.config.RpcConfig;
+import com.rpc.myrpc.utils.ConfigUtils;
+
+/**
+ * 简易服务消费者示例
+ *
+ */
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        System.out.println(rpc);
+    }
+}
+

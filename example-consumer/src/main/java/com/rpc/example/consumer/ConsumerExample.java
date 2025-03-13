5a5882a87817535dc7bca997e2d34380a34edfec
StarlitDreamer <3435185298@qq.com>
2025-03-13 15:09:54 +0800

    完成第三章

--- example-consumer/src/main/java/com/rpc/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/rpc/example/consumer/ConsumerExample.java
@@ -1,7 +1,8 @@
 package com.rpc.example.consumer;
 
-import com.rpc.myrpc.config.RpcConfig;
-import com.rpc.myrpc.utils.ConfigUtils;
+import com.rpc.example.common.model.User;
+import com.rpc.example.common.service.UserService;
+import com.rpc.myrpc.proxy.ServiceProxyFactory;
 
 /**
  * 简易服务消费者示例
@@ -10,8 +11,18 @@ import com.rpc.myrpc.utils.ConfigUtils;
 public class ConsumerExample {
 
     public static void main(String[] args) {
-        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
-        System.out.println(rpc);
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("yupi");
+
+        User newUser = userService.getUser(user);
+        if (newUser!=null){
+            System.out.println(newUser.getName());
+        }else {
+            System.out.println("user==null");
+        }
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
 
